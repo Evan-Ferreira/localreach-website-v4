36843cbc734bc736c3cb783d5d9453f007504c56
Evan Ferreira <evanjfer@gmail.com>
2025-03-09 13:33:01 -0400

    feat: finish left half of landing page

--- src/app/_sections/landing.tsx
+++ src/app/_sections/landing.tsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import Background from '../_components/landing/background';
+import Image from 'next/image';
+
 const Landing = () => {
     return (
         <div className="h-screen w-screen flex flex-row">
             <Background />
-            <div className="w-1/2 h-full ">
-                <h1>Unlock your TV Advertising</h1>
+            <div className="w-1/2 pl-36 flex flex-col justify-center gap-12 relative">
+                <div className="bg-[#FD6F2F] w-56 h-30 absolute top-34 -z-10"></div>
+                <h1 className="text-7xl font-weight-700">Unlock your</h1>
+                <h1 className="text-7xl font-weight-700 -mt-9">
+                    TV Advertising
+                </h1>
                 <div className="flex flex-row">
-                    <p>
+                    <p className="w-1/3">
                         Connect with local businesses through innovative
                         advertising solutions
                     </p>
-                    <p>
+                    <p className="w-1/3">
                         Connect with local businesses through innovative
                         advertising solutions
                     </p>
+                </div>
+                <div className="flex flex-row gap-4">
+                    <button className="bg-[#95D0FD] text-black px-10 py-2">
+                        Get Started
+                    </button>
+                    <Image
+                        src="/right-arrow.svg"
+                        alt="tv"
+                        width={50}
+                        height={50}
+                    />
                 </div>
             </div>
             <div className="w-1/2 h-full"></div>
