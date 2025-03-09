0239134b3631bbb997617343eee0c8e74c9b20b1
Evan Ferreira <evanjfer@gmail.com>
2025-03-09 16:17:20 -0400

    feat: add mobile view to landing page

--- src/app/_components/landing/venueScrollBar.tsx
+++ src/app/_components/landing/venueScrollBar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import Image from 'next/image';
 
 const VenueScrollBar = () => {
@@ -78,7 +78,7 @@ const VenueScrollBar = () => {
                     <div className="h-[200vh]"></div>
                 </div>
             </div>
-            <div className="h-full flex flex-col justify-center gap-8 items-center">
+            <div className="hidden lg:flex h-full w-full flex-col justify-center gap-8 items-center">
                 {[0, 1, 2, 3, 4].map((index) => (
                     <div
                         key={index}
