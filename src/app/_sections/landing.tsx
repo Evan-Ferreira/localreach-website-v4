08a77c4324e08a8bd192a7208ed3d3d197a43bb7
Evan Ferreira <evanjfer@gmail.com>
2025-03-09 13:16:27 -0400

    feat: background is separate component

--- src/app/_sections/landing.tsx
+++ src/app/_sections/landing.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
-
+import Background from '../_components/landing/background';
 const Landing = () => {
     return (
         <div className="h-screen w-screen flex flex-row">
-            <video
-                src={'landing-background.mp4'}
-                autoPlay
-                muted
-                loop
-                className="w-full absolute h-full object-cover"
-            />
-            <div className="w-full h-full absolute bg-[#080808] opacity-30"></div>
-            <div
-                className="absolute inset-0 bg-gradient-to-b from-transparent 
-                via-[#080808]/10 via-20%
-                via-[#080808]/25 via-35%
-                via-[#080808]/45 via-50%
-                via-[#080808]/65 via-65%
-                via-[#080808]/85 via-80%
-                to-[#080808]"
-            ></div>
-            <div className="w-1/2 h-full "></div>
-            <div></div>
+            <Background />
+            <div className="w-1/2 h-full ">
+                <h1>Unlock your TV Advertising</h1>
+                <div className="flex flex-row">
+                    <p>
+                        Connect with local businesses through innovative
+                        advertising solutions
+                    </p>
+                    <p>
+                        Connect with local businesses through innovative
+                        advertising solutions
+                    </p>
+                </div>
+            </div>
+            <div className="w-1/2 h-full"></div>
         </div>
     );
 };
