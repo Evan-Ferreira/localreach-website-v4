d8a659b843e50b64c5d360832cb96736fede86cd
Evan Ferreira <evanjfer@gmail.com>
2025-03-09 15:22:01 -0400

    fix: orange square for header text

--- src/app/_sections/landing.tsx
+++ src/app/_sections/landing.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import Background from '../_components/landing/background';
 import Image from 'next/image';
 import VenueScrollBar from '../_components/venueScrollBar';
+
 const Landing = () => {
     return (
         <div className="h-screen w-screen flex flex-row">
             <Background />
-            <div className="w-1/2 pl-36 flex flex-col justify-center gap-12 relative">
-                <div className="bg-[#FD6F2F] w-56 h-30 absolute top-34 -z-10"></div>
-                <h1 className="text-7xl font-weight-700">Unlock your</h1>
-                <h1 className="text-7xl font-weight-700 -mt-9">
-                    TV Advertising
-                </h1>
+            <div className="w-1/2 pl-36 flex flex-col justify-center gap-12">
+                <div className="relative">
+                    <div className="bg-[#FD6F2F] w-56 h-30 absolute -top-12 -z-10"></div>
+
+                    <h1 className="text-7xl font-weight-700">Unlock your</h1>
+                    <h1 className="text-7xl font-weight-700">TV Advertising</h1>
+                </div>
                 <div className="flex flex-row">
                     <p className="w-1/3">
                         Connect with local businesses through innovative
@@ -31,6 +33,7 @@ const Landing = () => {
                         alt="tv"
                         width={50}
                         height={50}
+                        className="cursor-pointer hover:translate-x-3 transition-all duration-300 ease-in-out"
                     />
                 </div>
             </div>
