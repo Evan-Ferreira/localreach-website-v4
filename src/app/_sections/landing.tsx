7bb4774bac492a43e68a4e1514632f3ce3e9482f
Evan Ferreira <evanjfer@gmail.com>
2025-03-10 00:30:38 -0400

    wip: spacing again, removed `` goober

--- src/app/_sections/landing.tsx
+++ src/app/_sections/landing.tsx
@@ -7,7 +7,7 @@ const Landing = () => {
     return (
         <div className="h-screen w-screen flex flex-row">
             <Background />
-            <div className="md:w-2/3 w-full pl-12 max-md:pr-12 md:pl-36 flex flex-col justify-center md:justify-end md:gap-12 gap-24 pb-12">
+            <div className="md:w-2/3 w-full pl-12 max-md:pr-12 md:pl-36 flex flex-col justify-center md:justify-end md:gap-8 gap-24 pb-12">
                 <div className="relative w-full">
                     <div
                         className="bg-[#FD6F2F] lg:w-63 lg:h-32 h-18 w-37 absolute lg:-top-8 -top-4 
@@ -21,7 +21,6 @@ const Landing = () => {
                         TV Advertising
                     </h1>
                 </div>
-                ``
                 <div className="flex flex-col md:flex-row max-md:gap-12">
                     <p className="w-full md:w-1/3 max-md:text-center">
                         Connect with local businesses through innovative
