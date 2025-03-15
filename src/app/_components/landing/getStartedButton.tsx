422442ba4c49abca752c4a2ed8528fd72496793e
Evan Ferreira <evanjfer@gmail.com>
2025-03-15 15:20:58 -0400

    fix: mobile view for get started button

--- src/app/_components/landing/getStartedButton.tsx
+++ src/app/_components/landing/getStartedButton.tsx
@@ -16,7 +16,7 @@ const GetStartedButton = () => {
         >
             <p
                 className="bg-[#95D0FD] text-[#080808] group-hover:text-[#FFFFFF] px-12 py-3 max-md:py-4 group-hover:bg-[#69a2cd] 
-                    cursor-pointer transition-all duration-300 ease-in-out max-md:w-4/5"
+                    cursor-pointer transition-all duration-300 ease-in-out text-center"
             >
                 Get Started
             </p>
