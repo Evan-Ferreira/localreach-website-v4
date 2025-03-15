8b40f97201519550f4fabf21380ea7a199161c15
Evan Ferreira <evanjfer@gmail.com>
2025-03-15 11:24:54 -0400

    fix: spacing on navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <>
             <div className="hidden lg:flex absolute w-full h-14 flex-row justify-center">
-                <div className="flex flex-row px-36 mt-12 justify-between 2xl:gap-38 gap-10 h-full w-full items-center">
+                <div className="flex flex-row px-36 mt-12 justify-between 2xl:gap-38 gap-24 h-full w-full items-center">
                     <img src="/lr-logo.svg" alt="logo" className="h-4/5" />
                     <div className="flex flex-row bg-[#FFFFFF4D] gap-6 px-16 w-full h-full rounded-full justify-around items-center">
                         <p className="2xl:text-lg text-md">Why Local Reach?</p>
